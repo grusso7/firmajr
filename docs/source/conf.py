05fea41a4bf764a7f65a4fb4608f6d8e6e147568
unknown <Giuseppe Russo@DESKTOP-37K0BUC.domain.ausl.bologna.it>
2020-02-03 18:08:52 +0100

    New rtd build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -65,9 +65,6 @@ exclude_patterns = []
 #
 html_theme = 'sphinx_rtd_theme'
 
-html_theme_options = {
-    'page_width': '70em'
-}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
